d6ca73f68e405c0b123fbceda04aba98dc647d9f
JWeez <jwellstx@gmail.com>
2020-06-07 19:52:03 -0500

    adding build folder

---
+++ build/precache-manifest.6bf7da9fa62b53c263aa54fb72eaff1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b02628548688aa13650fbfcddc7ef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b41895b762f107c424",
+    "url": "/static/css/main.8dfcccaa.chunk.css"
+  },
+  {
+    "revision": "e922c9332e9f3933105c",
+    "url": "/static/js/2.2828241f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2828241f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b41895b762f107c424",
+    "url": "/static/js/main.e2534920.chunk.js"
+  },
+  {
+    "revision": "91d3592f934283477157",
+    "url": "/static/js/runtime-main.1c5d0ecf.js"
+  }
+]);
