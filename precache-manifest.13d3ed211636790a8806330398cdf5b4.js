c3f374f95a679a28023f60268a966c76874de8a8
JWeez <jwellstx@gmail.com>
2020-06-07 20:55:06 -0500

    Updates

---
+++ precache-manifest.13d3ed211636790a8806330398cdf5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5ad4674ac2e283f68942e0270cb823",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "471f18c12a05986dd29b",
+    "url": "/clickygame/static/css/main.8dfcccaa.chunk.css"
+  },
+  {
+    "revision": "e922c9332e9f3933105c",
+    "url": "/clickygame/static/js/2.2828241f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.2828241f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471f18c12a05986dd29b",
+    "url": "/clickygame/static/js/main.22670655.chunk.js"
+  },
+  {
+    "revision": "c798aa2c017665a2cf0e",
+    "url": "/clickygame/static/js/runtime-main.8918f080.js"
+  }
+]);
